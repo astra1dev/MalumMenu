5b69c7cda8e707b32665236bface354de7b75189
Astral✨ <90265231+astra1dev@users.noreply.github.com>
2025-12-16 02:13:41 +0100

    refactor: use StringNames and simplify logic

--- src/Patches/NumberOptionPatches.cs
+++ src/Patches/NumberOptionPatches.cs
@@ -15,16 +15,12 @@ public static class NumberOption_Increase
     public static bool Prefix(NumberOption __instance)
     {
         if (!CheatToggles.noOptionsLimits) return true;
-        
-        if (Utils.isHideNSeek || !Utils.isHideNSeek && __instance.TitleText.text != "# Impostors" && __instance.TitleText.text != "Player Speed")
-        {
-            __instance.Value +=  __instance.Increment;
-            __instance.UpdateValue();
-            __instance.OnValueChanged.Invoke(__instance);
-            __instance.AdjustButtonsActiveState();
-            return false;
-        }
-        else return true;
+        if (!Utils.isHideNSeek && __instance.Title is StringNames.GameNumImpostors or StringNames.GamePlayerSpeed) return true;
+        __instance.Value += __instance.Increment;
+        __instance.UpdateValue();
+        __instance.OnValueChanged.Invoke(__instance);
+        __instance.AdjustButtonsActiveState();
+        return false;
     }
 }
 
@@ -39,16 +35,12 @@ public static class NumberOption_Decrease
     public static bool Prefix(NumberOption __instance)
     {
         if (!CheatToggles.noOptionsLimits) return true;
-        
-        if (Utils.isHideNSeek || !Utils.isHideNSeek && __instance.TitleText.text != "# Impostors" && __instance.TitleText.text != "Player Speed")
-        {
-            __instance.Value -=  __instance.Increment;
-            __instance.UpdateValue();
-            __instance.OnValueChanged.Invoke(__instance);
-            __instance.AdjustButtonsActiveState();
-            return false;
-        }
-        else return true;
+        if (!Utils.isHideNSeek && __instance.Title is StringNames.GameNumImpostors or StringNames.GamePlayerSpeed) return true;
+        __instance.Value -= __instance.Increment;
+        __instance.UpdateValue();
+        __instance.OnValueChanged.Invoke(__instance);
+        __instance.AdjustButtonsActiveState();
+        return false;
     }
 }
 
@@ -62,10 +54,7 @@ public static class NumberOption_Initialize
     public static void Postfix(NumberOption __instance)
     {
         if (!CheatToggles.noOptionsLimits) return;
-
-        if (Utils.isHideNSeek || !Utils.isHideNSeek && __instance.TitleText.text != "# Impostors" && __instance.TitleText.text != "Player Speed")
-        {
-            __instance.ValidRange = new FloatRange(-999f, 999f);
-        }
+        if (!Utils.isHideNSeek && __instance.Title is StringNames.GameNumImpostors or StringNames.GamePlayerSpeed) return;
+        __instance.ValidRange = new FloatRange(-999f, 999f);
     }
 }
