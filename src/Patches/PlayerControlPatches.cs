ad6e5c0049910b9ac74b29ed89dc2b85ae6e3db9
Astral✨ <90265231+astra1dev@users.noreply.github.com>
2026-02-11 16:44:27 +0000

    Revert "fix merge conflicts part 1"
    This reverts commit 9683f5ca89c45c1ccb4af7b183e58a540bc4d68f.

--- src/Patches/PlayerControlPatches.cs
+++ src/Patches/PlayerControlPatches.cs
@@ -99,10 +99,7 @@ public static class PlayerControl_CmdCheckMurder
 [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.TurnOnProtection))]
 public static class PlayerControl_TurnOnProtection
 {
-    /// <summary>
-    /// Prefix patch of PlayerControl.TurnOnProtection to render all protections visible if CheatToggles.seeGhosts is enabled
-    /// </summary>
-    /// <param name="visible">Whether the protection should be visible.</param>
+    // Prefix patch of PlayerControl.ProtectPlayer to render all protections visible if CheatToggles.seeGhosts is enabled
     public static void Prefix(ref bool visible){
         if (CheatToggles.seeGhosts){
             visible = true;
